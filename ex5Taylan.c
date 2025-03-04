894da4132184ab7d783546886c0cf764008fe8e1
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-04 20:18:17 -0300

    Add files via upload

---
+++ ex5Taylan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float real, cotacaoDolar, conversao;
+    
+    printf("Câmbio dos Guris");
+    printf("\nDigite o valor em real: ");
+    scanf("%f", &real);
+    printf("\nDigite o valor da cotação atual do dólar: ");
+    scanf("%f", &cotacaoDolar);
+    
+    conversao = real * cotacaoDolar;
+    printf("\nO valor R$ %.2f foi convertido para %.2f", real, conversao);
+}
+
+// %.2f - para limitar o número de casas decimais
