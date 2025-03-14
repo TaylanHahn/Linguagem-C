971babd0410cbd5c7c0d5f7eb0f21898214a9bfb
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-14 19:44:14 -0300

    Update ifelse3.c

--- exercicios if else/ifelse3.c
+++ exercicios if else/ifelse3.c
@@ -3,21 +3,19 @@
 int main() {
     float media;
     int faltas;
-    
-    // Leitura da média e do número de faltas
+
     printf("Digite a média do aluno: ");
     scanf("%f", &media);
     
     printf("Digite o número de faltas: ");
     scanf("%d", &faltas);
     
-    // Verificação das condições de aprovação
     if (faltas >= 5) {
-        printf("Reprovado por faltas\n");
+        printf("\nVocê reprovou por faltas");
     } else if (media < 7.0) {
-        printf("Reprovado por média\n");
+        printf("\nVocê reprovou por média");
     } else {
-        printf("Parabéns! Você foi aprovado\n");
+        printf("\nParabéns! Você foi aprovado");
     }
     
     return 0;
