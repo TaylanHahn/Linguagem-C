a869910e0e41b04f360acf4c282c185ba36f722a
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-14 19:53:03 -0300

    Update ifelse6.c

--- exercicios if else/ifelse6.c
+++ exercicios if else/ifelse6.c
@@ -3,18 +3,15 @@
 int main() {
     int numero;
     
-    // Leitura do número
     printf("Digite um número inteiro: ");
     scanf("%d", &numero);
     
-    // Verificação se é par ou ímpar
     if (numero % 2 == 0) {
         printf("O número %d é par", numero);
     } else {
         printf("O número %d é ímpar", numero);
     }
     
-    // Verificação se é positivo ou negativo
     if (numero >= 0) {
         printf(" e é positivo.\n");
     } else {
