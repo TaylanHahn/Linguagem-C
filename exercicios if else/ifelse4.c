9869cc2dd5fc1fb2e3f0dcbaef9c17951d8c72b4
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-14 19:47:23 -0300

    Update ifelse4.c

--- exercicios if else/ifelse4.c
+++ exercicios if else/ifelse4.c
@@ -3,23 +3,21 @@
 int main() {
     int idade;
     
-    // Leitura da idade do nadador
     printf("Digite a idade do nadador: ");
     scanf("%d", &idade);
     
-    // Classificação por categoria
     if (idade >= 5 && idade <= 7) {
-        printf("Categoria: Infantil A\n");
+        printf("\nCategoria: Infantil A");
     } else if (idade >= 8 && idade <= 10) {
-        printf("Categoria: Infantil B\n");
+        printf("\nCategoria: Infantil B");
     } else if (idade >= 11 && idade <= 13) {
-        printf("Categoria: Juvenil A\n");
+        printf("\nCategoria: Juvenil A");
     } else if (idade >= 14 && idade <= 17) {
-        printf("Categoria: Juvenil B\n");
+        printf("\nCategoria: Juvenil B");
     } else if (idade >= 18) {
-        printf("Categoria: Adulto\n");
+        printf("\nCategoria: Adulto");
     } else {
-        printf("Idade fora das categorias classificadas\n");
+        printf("\nIdade fora das categorias classificadas");
     }
     
     return 0;
