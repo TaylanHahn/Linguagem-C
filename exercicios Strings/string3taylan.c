99c425d82df2bda15148df083836bc2791ac0172
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-21 19:24:52 -0300

    Add files via upload

---
+++ exercicios Strings/string3taylan.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+	char nome [100];
+	int idade;
+
+	printf("Digite seu nome completo: ");
+	scanf(" %[^\n]s", nome);
+	printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    
+    if(idade < 18){
+        printf("%s é menor de idade", nome);
+    } else{
+        printf("%s é maior de idade", nome);
+    }
+}
