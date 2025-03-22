f788146c2b7fa073d84e7f890b30bedebb1dc0b4
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-03-21 21:11:56 -0300

    Add files via upload

---
+++ exercicios Strings/string4taylan.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int faltas;
+    float nota1, nota2, trabalho, media;
+    
+    printf("Insira a primeira nota: \n");
+    scanf("%f", &nota1);
+    printf("Insira a segunda nota: \n");
+    scanf("%f", &nota2);
+    printf("Insira a nota do trabalho: \n");
+    scanf("%f", &trabalho);
+    printf("Número de faltas: \n");
+    scanf("%d", &faltas);
+    
+    media = (nota1 + nota2 + trabalho) / 3;
+    printf("Sua média é de:%.2f\n", media);
+    printf("-----------------------------");
+    
+    
+    if(faltas >= 5){
+        printf("Reprovado por faltas\n");
+    } else if(media >= 8){
+        printf("\nAvaliação A");
+    } else if(media >= 7 && media <= 7.9){
+        printf("\nAvaliação B");
+    } else if (media >= 6 && media <= 6.9){
+        printf("\nAvaliação C");
+    } else if (media >= 0 && media <= 5.9){
+        printf("\nAvaliação D");
+    } 
+    
+}
