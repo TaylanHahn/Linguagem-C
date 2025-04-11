da3bb9af8a32bbb01943b1231d7ca903f600f62c
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:36:44 -0300

    Update switch01_taylan.c

--- SWITCH/switch01_taylan.c
+++ SWITCH/switch01_taylan.c
@@ -1,4 +1,17 @@
+/**********************************************************************************************************
+01) Em uma central de entregas, os pacotes são separados de acordo com o CEP de destino.
+Escreva um programa que leia do usuário o primeiro dígito do CEP de destino e informe
+para qual região ele deve ser enviado, de acordo com a tabela abaixo:
 
+Primeiro Dígito do CEP	Enviar para: 
+0, 1, 2, 3	Sudeste (SP, RJ, ES, MG)
+4	Região 4 (BA, SE)
+5	Região 5 (PE, AL, PB, RN)
+6	Região 6 (CE, PI, MA, PA, AP, AM, RR, AC )
+7	Região 7 (DF, GO, TO, MT, RO, MS)
+8 ou 9 	Sul (PR, SC, RS)
+Ex: se o código digitado for 5, o programa deve imprimir em tela: "Enviar para: Região 5 (PE, AL, PB, RN)"
+**********************************************************************************************************/
 #include <stdio.h>
 
 int main()
