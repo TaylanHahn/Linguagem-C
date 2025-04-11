54bbf8437edaab7d682ecec618b6f30c19c3a0b4
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:31:50 -0300

    Update laco5.c

--- ex. LacosRepeticao/laco5.c
+++ ex. LacosRepeticao/laco5.c
@@ -2,7 +2,6 @@
 5)Contagem com decremento
 Escreva um programa que imprima os números de 1 a 10 na tela, em ordem crescente,
 utilizando um comando for com decremento (ou seja, for(i = 10; i > 0; i--)).
-
 **********************************************************************************************************/
 #include <stdio.h>
 
