77268011ee91fa4de8815d497e7e94a42dde7616
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:43:52 -0300

    Update string1taylan.c

--- exercicios Strings/string1taylan.c
+++ exercicios Strings/string1taylan.c
@@ -1,3 +1,8 @@
+/**********************************************************************************************************
+1) Escreva um programa que leia o nome do usuário e informe quantos caracteres o nome ocupa (strlen).
+Informe a capacidade da variável com o comando sizeof.
+**********************************************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 
