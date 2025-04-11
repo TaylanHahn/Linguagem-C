bfb99fa03ccc628d5d896b69f599e62b44d4d886
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:31:42 -0300

    Update laco4.c

--- ex. LacosRepeticao/laco4.c
+++ ex. LacosRepeticao/laco4.c
@@ -2,7 +2,6 @@
 4)Múltiplos de 3 com DO WHILE decrescente
 Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem decrescente, utilizando o laço do while.
 Dica: comece de 18 e vá até 3.
-
 **********************************************************************************************************/
 
 #include <stdio.h>
