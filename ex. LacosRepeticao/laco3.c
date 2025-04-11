119217e0a6e1b3de7e5fa4bad776116c99b8a200
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:33:23 -0300

    Update laco3.c

--- ex. LacosRepeticao/laco3.c
+++ ex. LacosRepeticao/laco3.c
@@ -1,6 +1,8 @@
-// 3) MC:ltiplos de 3 com WHILE: Escreva um programa que imprima os multiplos de 3 entre 1 e 20, em ordem crescente, utilizando o laço while.
-// Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
-
+/**********************************************************************************************************
+3) Múltiplos de 3 com WHILE
+Escreva um programa que imprima os múltiplos de 3 entre 1 e 20, em ordem crescente, utilizando o laço while.
+Dica: o primeiro múltiplo de 3 dentro do intervalo é o número 3.
+**********************************************************************************************************/
 #include <stdio.h>
 
 int main(){
