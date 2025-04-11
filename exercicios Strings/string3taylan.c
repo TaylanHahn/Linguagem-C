71cb36716d39e056f87072729f32bb44d2d3635c
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 20:46:41 -0300

    Update string3taylan.c

--- exercicios Strings/string3taylan.c
+++ exercicios Strings/string3taylan.c
@@ -1,3 +1,7 @@
+/**********************************************************************************************************
+3) Escreva um programa que leia o nome e a idade do usuário e retorne uma mensagem indicando se o usuário é maior de idade ou não. 
+Vamos considerar 18 anos completos. Ex: "FULANO é maior de idade" ou "FULANO é Menor de idade"
+**********************************************************************************************************/
 
 #include <stdio.h>
 
