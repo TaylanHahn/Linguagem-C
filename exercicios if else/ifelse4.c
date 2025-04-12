5ec21a19b9b4c745c46b9da54a357e874790ae8e
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 21:02:35 -0300

    Update ifelse4.c

--- exercicios if else/ifelse4.c
+++ exercicios if else/ifelse4.c
@@ -1,3 +1,12 @@
+/**********************************************************************************************************
+Exercício 4. - Escreva um programa que, dada a idade de um nadador, classifique-o em uma das seguintes categorias:
+infantil A = 5 - 7 anos
+infantil B = 8-10 anos
+juvenil A = 11-13 anos
+juvenil B = 14-17 anos
+adulto = 18 anos ou mais
+**********************************************************************************************************/
+
 #include <stdio.h>
 
 int main() {
