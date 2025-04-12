1297a30095c140773b9b279f7a88117e9b1467e5
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-04-11 21:08:45 -0300

    Update ex8Taylan.c

--- exercícios básicos/ex8Taylan.c
+++ exercícios básicos/ex8Taylan.c
@@ -1,3 +1,21 @@
+/**********************************************************************************************************
+Exercício 8. Equidade: Léia e Luke são irmãos e dividem um apartamento alugado. 
+O salário de Luke é R$2.000,00 o salário de Léia é de R$4.000,00. 
+O valor do aluguel é de R$1.000,00. 
+Para que a divisão do valor tenha equidade, Luke deve pagar 1/3 e Léia deve pagar 2/3. 
+Para calcular essa proporção, basta dividir o salário de cada um pela soma dos salários.
+
+Proporção Luke será de 2000/6000 = 1/3
+Proporção Léia será de 4000/6000 = 2/3
+
+Valor a pagar no aluguel: Luke R$333,33 Léia=R$666,67
+
+Escreva um programa que, leia o salário de 2 pessoas e o valor da conta a ser dividida.
+Apresente quanto cada um deve pagar em uma divisão com equidade.
+
+Após calcular a proporção da pessoa, basta multiplicar pelo valor da conta, que resultará no valor a ser pago pela pessoa.
+**********************************************************************************************************/
+
 #include <stdio.h>
 
 int main()
